a0fedf4ca61e83135104e67c8315af2a2a5702ab
Kevin Bai <lkevin957@gmail.com>
2019-02-26 14:11:54 -0800

    Add document loaded event listener

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,4 @@
 //= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
-//= require_tree.
+//= require_tree .
---
+++ frontend/Augustus.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const root = document.getElementById('root');
+//     ReactDOM.render( <h1>Hi</h1>, root )
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rootEl = document.getElementById('root');
+    ReactDOM.render(<h1>Root Placeholder</h1>, rootEl);
+});
