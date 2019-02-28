ed3d77a27d23e5e6d3078cd6a7a9834c7b9964cf
Kevin Bai <lkevin957@gmail.com>
2019-02-28 15:26:37 -0800

    Add rough styling for the layout of the notes page.

--- frontend/components/ink/ink.jsx
+++ frontend/components/ink/ink.jsx
@@ -3,8 +3,42 @@ import { Link } from 'react-router-dom';
 
 const Ink = (props) => {
     return (
-        <div className="Ink" >
-            <h1>I AM INK</h1>
+        <div className="bars">
+            <div className="lbar bar">
+                <h1>User Info</h1>
+                <h1>Search Bar</h1>
+                <h1>Add New Note</h1>
+                <ul>
+                    <li>Shortcuts</li>
+                    <li>All Notes</li>
+                    <li>Notebooks</li>
+                    <li>Shared with Me</li>
+                    <li>Tags</li>
+                    <li>Trash</li>
+                    <li>Premium</li>
+                </ul>
+            </div>
+            <div className="mbar bar">
+                <h1>All Notes</h1>
+                <ul>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                    <li>text</li>
+                </ul>
+            </div>
+            <div className="content bar">
+                <h1>Main Nav</h1>
+                <p>
+                    content goes here
+                </p>
+                <h1>Tags</h1>
+            </div>
         </div>
     )
 };
