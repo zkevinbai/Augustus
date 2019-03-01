956e9e4be13599889e27e470b8690f89e44903d2
Kevin Bai <lkevin957@gmail.com>
2019-02-28 21:39:27 -0800

    Add dropdown logout, add listeners to search, new note, shortcuts, all notes, notebooks, shared with, tags, trash, and premium

--- frontend/components/ink/ink.jsx
+++ frontend/components/ink/ink.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class Ink extends React.Component{
     constructor(props){
         super(props);
-        this.state = {hidden: false};
+        this.state = {hidden: true};
         this.hide = this.hide.bind(this);
     }
 
@@ -21,22 +21,71 @@ class Ink extends React.Component{
                 <a onClick={this.hide}>                            
                     <img src={window.logoURL} />
                     <h1>{this.props.user.username}</h1>
-                    <div style={hidden} className="dropDiv" >
-                        <button onClick={() => this.props.logout()}>Logout</button>
-                    </div>
+                    <i className="fas fa-angle-down"></i>
+                </a>
+                <div style={hidden} className="dropDiv" >
+                    <button onClick={() => this.props.logout()}>Logout</button>
+                </div>
+            </div>
+
+            <div className="searchBar" >
+                <a onClick={() => console.log("click")}> 
+                    <span class="searchbar">
+                        <input type="text" placeholder="Search all notes"/>
+                        <button class="fas fa-search"></button>
+                    </span>
                 </a>
             </div>
-            <h1>Search Bar</h1>
-            <h1>Add New Note</h1>
-            <ul>
-                <li>Shortcuts</li>
-                <li>All Notes</li>
-                <li>Notebooks</li>
-                <li>Shared with Me</li>
-                <li>Tags</li>
-                <li>Trash</li>
-                <li>Premium</li>
-            </ul>
+
+            <div className="newNote" >
+                <a onClick={() => console.log("click")}> 
+                    <i className="fas fa-feather-alt"></i>
+                    <h1>Add New Note</h1>
+                </a>
+            </div>
+            
+            <div className="shortcuts" >
+                <a onClick={() => console.log("click")}> 
+                    <h1>Shortcuts</h1>
+                </a>
+            </div>
+
+            <div className="allNotes" >
+                <a onClick={() => console.log("click")}> 
+                    <h1>All Notes</h1>
+                </a>
+            </div>
+
+            <div className="Notebooks" >
+                <a onClick={() => console.log("click")}> 
+                    <h1>Notebooks</h1>
+                </a>
+            </div>
+
+            <div className="sharedWith" >
+                <a onClick={() => console.log("click")}>
+                    <h1>Shared with Me</h1>
+                </a>
+            </div>
+
+            <div className="Tags" >
+                <a onClick={() => console.log("click")}>
+                    <h1>Tags</h1>
+                </a>
+            </div>
+
+            <div className="Trash" >
+                <a onClick={() => console.log("click")}>
+                    <h1>Trash</h1>
+                </a>
+            </div>
+
+            <div className="Premium" >
+                <a onClick={() => console.log("click")}>
+                    <h1>Premium</h1>
+                </a>
+            </div>
+            
         </div>
         <div className="mbar bar">
             <h1>All Notes</h1>
