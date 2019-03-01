86513db266d5a613196da39a5cad3638281d5c74
Kevin Bai <lkevin957@gmail.com>
2019-03-01 11:29:34 -0800

    Restyle landing photo to be consitent with login

--- frontend/Augustus.jsx
+++ frontend/Augustus.jsx
@@ -29,7 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const store = configureStore(preLoadedState);
 
-
     // #########################################################################
         // ---------------------------TESTING START-----------------------------
 
