44f47f534ffd87aef4f29f5659819cad1ce823d3
Kevin Bai <lkevin957@gmail.com>
2019-03-03 17:18:58 -0800

    Major Styling of Notebooks index page, complete styling of header, categories, and notebooks proper

--- frontend/components/ink/notebooks/MR_notebooks.index.jsx
+++ frontend/components/ink/notebooks/MR_notebooks.index.jsx
@@ -11,17 +11,28 @@ class LeftNotebooksIndex extends React.Component {
             <div className={`mr-NotebooksIndex`}>
                 <div className={`mr-NotebooksIndex-heading`}>
                     <h1>Notebooks</h1>
-                    <a><h1>New Notebook</h1></a>
+                    <span className="newNotebook">
+                        <i className="fas fa-book-medical"></i>
+                        <h1>New</h1>
+                    </span>
                 </div>
                 <div className={`mr-NotebooksIndex-categories`}>
                     <h1>Title</h1>
+                    <h1>Edit</h1>
+                    <h1>Delete</h1>
                 </div>
                 <div className={`mr-NotebooksIndex-notebooks`}>
                     {this.props.notebooks.map(notebook => (
-                        <Link to={`/notebook/${notebook.id}`} key={notebook.id} >
-                            <i className="fas fa-book"></i>
-                            <h2>{notebook.notebook_title}</h2>
-                        </Link>
+                        <span>
+                            <Link to={`/notebook/${notebook.id}`} key={notebook.id} >
+                                <i className="fas fa-book"></i>
+                                <h2>{notebook.notebook_title}</h2>
+                            </Link>
+                            <i className="fas fa-edit notebookEdit"
+                            ></i>
+                            <i className="fas fa-book-dead notebookDelete"
+                            ></i>
+                        </span>
                     )
                     )}
                 </div>
