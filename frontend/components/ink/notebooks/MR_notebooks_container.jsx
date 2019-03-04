e164b3a8695c153c420bc38707b46bc225104369
Kevin Bai <lkevin957@gmail.com>
2019-03-04 09:07:02 -0800

    Refactor notebook file names

--- frontend/components/ink/notebooks/MR_notebooks_container.jsx
+++ frontend/components/ink/notebooks/MR_notebooks_container.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import MRNotebooksIndex from './MR_notebooks.index';
+import MRNotebooksIndex from './MR_notebooks_index';
 import {
     notebooksIndex,
     deleteNotebook
