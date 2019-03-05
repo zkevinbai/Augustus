17cc0863e48e356c44b85eebd9222b99890e647a
Kevin Bai <lkevin957@gmail.com>
2019-03-05 09:29:24 -0800

    Update entities reducer and errors reducer to have notes reducer and notes error reducers  respectively

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './entities/users_reducer';
 import notebooksReducer from './entities/notebooks_reducer';
+import notesReducer from './entities/notes_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    notebooks: notebooksReducer
+    notebooks: notebooksReducer,
+    notes: notesReducer
 }); 
 
 export default entitiesReducer;
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import sessionErrorsReducer from './errors/session_errors_reducer';
 import notebook_errors_reducer from './errors/notebook_errors_reducer';
+import note_errors_reducer from './errors/note_errors_reducer';
 
 const errorsReducer = combineReducers({
     session: sessionErrorsReducer,
-    notebooks: notebook_errors_reducer
+    notebooks: notebook_errors_reducer,
+    notes: note_errors_reducer,
 });
 
 export default errorsReducer;
