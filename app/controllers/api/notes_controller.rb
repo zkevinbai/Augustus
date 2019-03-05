23ff615cf4937a0b414d8401618878ac93b39844
Kevin Bai <lkevin957@gmail.com>
2019-03-04 20:42:36 -0800

    Add controller actions for note to show, create, update, and destroy

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -9,4 +9,43 @@ class Api::NotesController < ApplicationController
         render :index
     end
 
+    def show
+        @note = Note.find(params[:id])
+
+        render :show
+    end
+
+    def create
+        @note = Note.new(note_params)
+
+        if @note.save 
+            render :show
+        else
+            render json: @note.errors.full_messages, status: 404
+        end
+    end
+
+    def update 
+        @note = Note.find(params[:id])
+
+        if @note.update(note_params) 
+            render :show
+        else
+            render json: @note.errors.full_messages, status: 404
+        end
+    end
+
+    def destroy
+        @note = Note.find(params[:id])
+
+        if @note.destroy
+            render json: {}, status: 200
+        end
+    end
+
+    private 
+
+    def note_params 
+        params.require(:note).permit(:note_title, :note_body, :notebook_id)
+    end
 end
