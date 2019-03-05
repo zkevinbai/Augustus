e14b51fbd997439ba0d7d68c7c7beec937685576
Kevin Bai <lkevin957@gmail.com>
2019-03-04 21:42:04 -0800

    Add note api utils for index, show create, update, delete

---
+++ frontend/util/note_api_util.js
@@ -0,0 +1,40 @@
+export const notesIndex = () => {
+    return $.ajax({
+        type: "GET",
+        url: `api/notes`
+    });
+};
+
+export const noteShow = (id) => {
+    return $.ajax({
+        type: "GET",
+        url: `api/notes/${id}`
+    });
+};
+
+export const createNote = (note) => {
+    return $.ajax({
+        type: "POST",
+        url: `api/notes`,
+        data: {
+            note
+        }
+    });
+};
+
+export const updateNote = (note) => {
+    return $.ajax({
+        type: "PATCH",
+        url: `api/notes/${note.id}`,
+        data: {
+            note
+        }
+    });
+};
+
+export const deleteNote = (id) => {
+    return $.ajax({
+        type: "DELETE",
+        url: `api/notes/${id}`
+    });
+};
