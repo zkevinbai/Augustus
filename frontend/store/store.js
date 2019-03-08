877877f0609f77c6bf4ec4f0d36bcd6b31d64d46
Kevin Bai <lkevin957@gmail.com>
2019-03-08 09:47:26 -0800

    Refactor store to remove logger for production app

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,12 +3,25 @@ import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-const configureStore = (preLoadedState = {}) => (
-    createStore(
-        rootReducer,
-        preLoadedState,
-        applyMiddleware(thunk, logger)
-    )
-);
+// const configureStore = (preLoadedState = {}) => (
+//     createStore(
+//         rootReducer,
+//         preLoadedState,
+//         applyMiddleware(thunk, logger)
+//     )
+// );
 
+// export default configureStore;
+
+const configureStore = (preloadedState = {}) => {
+    let middleware = [thunk];
+    if (process.env.NODE_ENV !== 'production') {
+        middleware = [...middleware, logger];
+    }
+    return createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(...middleware)
+    );
+};
 export default configureStore;
