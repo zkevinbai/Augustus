11166fd321aa92eb1ad730365a398a55378bfd62
Kevin Bai <lkevin957@gmail.com>
2019-03-07 21:37:05 -0800

    Add how to use seed into demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ ActiveRecord::Base.transaction do
         password: "password",
     )
 
-    demo_programming = Notebook.create(
+    demo_personal = Notebook.create(
         user_id: demo_user.id, 
         notebook_title: "Personal"
     )
@@ -43,5 +43,12 @@ ActiveRecord::Base.transaction do
     demo_history = Notebook.create(
         user_id: demo_user.id, 
         notebook_title: "Travel"
+    )
+
+    demo_note = Note.create(
+        note_title: "How to",
+        note_body:
+        "<h1>New Note</h1><ol><li>click add new note button</li><li>click any notebook</li><li>write as you'd like then click the disc to save it to the database</li></ol><h1>Edit Note</h1><ol><li>click a note from the side bar</li><li>edit as you'd like and then click the disc to save it to the database</li></ol><h1>Delete Note</h1><ol><li>delete notes by clicking the skull and crossbones to the upper right hand corner of the note</li></ol><h1>Notebook</h1><ol><li>click notebook chevron for notebook dropdown</li><li>click notebooks button to access notebook interface</li><li>add, edit, or delete a notebook at the edit notebook interface</li></ol><h1>Augustus Editor</h1><ol><li>Supports photos and links to videos</li><li>Supports rich text editing</li></ol><p><br></p>",
+        notebook_id: demo_personal.id
     )
 end
